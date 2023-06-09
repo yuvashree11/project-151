0df1fe115eca3be1f060a0c8bc35c305a27ae9a2
yuvashree11 <81632676+yuvashree11@users.noreply.github.com>
2023-06-09 21:55:13 +0530

    Add files via upload

---
+++ base.js
@@ -0,0 +1,17 @@
+AFRAME.registerComponent("base", {
+    schema: {
+      radius: { type: "number", default: 150 },  
+      height: { type: "number", default: 3 }    
+    },
+
+    init:function(){
+        this.el.setAttribute("geometry",{
+            primitive:"cylinder",
+            radius:this.data.radius,
+            height:this.data.height
+        });
+        this.el.setAttribute("material",{color: "#769aa"});
+    }
+
+});
+     
